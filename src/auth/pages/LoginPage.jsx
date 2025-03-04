673d43b479dcb948be81bebfe4e9e312ab77f196
Ernest079 <ernestomendoza.martinez73@gmail.com>
2025-03-04 11:33:14 -0600

    Hero List

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,24 @@
+import { useNavigate } from "react-router"
 
 export const LoginPage = () => {
+  const naviagte = useNavigate();
+
+  const onLogin = () => {
+    naviagte('/', {
+      replace: true,
+    });
+  }
+
   return (
-    <h1>LoginPage</h1>
+    <div className="container mt-5">
+      <hr />
+
+      <button className="btn btn-primary"
+        onClick={onLogin}
+      >
+
+        Login
+      </button>
+    </div>
   )
 }
